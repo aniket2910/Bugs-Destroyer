af6d0d2177db5387060705bb7022beb5d1d128f0
Debobrota Haldar <99647439+iamdebobrota@users.noreply.github.com>
2022-07-28 21:18:18 +0530

    debu init
    debu init

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,28 @@
+import Login from './pages/signup/Login'
+import { extendTheme, ChakraProvider } from '@chakra-ui/react';
+import SignupCard from './pages/signup/Signup';
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
+
 function App() {
-  return <div></div>;
+  return(
+  <ChakraProvider theme={theme}>
+  <div>
+
+    <Login/>
+    <SignupCard/>
+  </div>
+  </ChakraProvider>
+  );
 }
 
 export default App;
