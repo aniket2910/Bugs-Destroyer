2832b3164ec4cabdc5560a9b180163b0949b94e5
nitinkadam70 <99539536+nitinkadam70@users.noreply.github.com>
2022-07-30 02:29:47 +0530

    Dashboard is done now

--- client/src/components/DashBord/ProgressBar.jsx
+++ client/src/components/DashBord/ProgressBar.jsx
@@ -1,13 +1,18 @@
 import {
+    Avatar,
     Box,
     chakra,
     CircularProgress,
     CircularProgressLabel,
     Flex,
+    HStack,
     SimpleGrid,
     Stat,
     StatLabel,
     StatNumber,
+    Tag,
+    TagLabel,
+    TagLeftIcon,
     Text,
     useColorModeValue,
 } from '@chakra-ui/react';
@@ -18,11 +23,12 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import { getTodosData } from '../../redux/Todos/actions';
 import { useState } from 'react';
+import { AddIcon } from '@chakra-ui/icons';
 
 
 
 function StatsCard(props) {
-    const { title, stat, icon, bg, iconColor, percentile } = props;
+    const { title, stat, icon, bg, iconColor, percentile, progress } = props;
     return (
         <Stat
             bg={bg}
@@ -41,11 +47,18 @@ function StatsCard(props) {
                         {stat}
                     </StatNumber>
                 </Box>
-                <Box my={'auto'} alignContent={'center'}>
+                {progress ? <Box my={'auto'} alignContent={'center'}>
                     <CircularProgress value={percentile} color='green.400'>
                         <CircularProgressLabel>{percentile}%</CircularProgressLabel>
                     </CircularProgress>
-                </Box>
+                </Box> :
+                    <HStack spacing={4}>
+                        <Tag size={'lg'} variant='subtle' colorScheme='pink'>
+                            <TagLeftIcon boxSize='12px' as={AddIcon} />
+                            <TagLabel>Do your best</TagLabel>
+                        </Tag>
+                    </HStack>
+                }
                 <Box
                     my={'auto'}
                     color={useColorModeValue(iconColor)}
@@ -61,7 +74,10 @@ export default function ProgressBar() {
 
     const dispatch = useDispatch();
     const { lading, todos, error } = useSelector((store) => store.todos);
-
+    const [todosData, setTodosData] = useState(0);
+    const [all, setAll] = useState(0)
+    const [todosDoneData, setTodosDoneData] = useState(0);
+    const [todosInProgressData, setTodosInProgresData] = useState(0);
 
     useEffect(() => {
         if (todos.length <= 0) {
@@ -69,17 +85,35 @@ export default function ProgressBar() {
         }
     }, [dispatch])
 
-    var i = 0;
-    var j = 0;
-    var k = 0;
+
     useEffect(() => {
-        let data = todos.data && todos.data.map((elem) => {
+        let data = todos.data && todos.data.filter((elem, index) => {
             if (elem.status.todo && elem.delete === false) {
-                return elem
+                return { ...elem }
             }
         })
+        setTodosData(data && data.length)
 
-        console.log("data", data)
+        let data4 = todos.data && todos.data.filter((elem, index) => {
+            if (elem.delete === false) {
+                return { ...elem }
+            }
+        })
+        setAll(data4 && data4.length)
+
+        let data2 = todos.data && todos.data.filter((elem, index) => {
+            if (elem.status.inPogress && elem.delete === false) {
+                return { ...elem }
+            }
+        })
+        setTodosInProgresData(data2 && data2.length)
+
+        let data3 = todos.data && todos.data.filter((elem, index) => {
+            if (elem.status.done && elem.delete === false) {
+                return { ...elem }
+            }
+        })
+        setTodosDoneData(data3 && data3.length)
     }, [todos.data])
 
     return (
@@ -88,20 +122,24 @@ export default function ProgressBar() {
                 <StatsCard
                     percentile='100'
                     title={'To do'}
-                    stat={i}
+                    stat={todosData}
                     icon={<RiTodoLine size={'3em'} />}
                 />
                 <StatsCard
-                    percentile='20'
+
+                    percentile={Math.ceil((todosInProgressData / all) * 100)}
                     title={'In progress'}
-                    stat={j}
+                    stat={todosInProgressData}
                     icon={<FiServer size={'3em'} />}
+                    progress={true}
                     iconColor={'pink.400'}
                 />
 
                 <StatsCard
+                    percentile={Math.ceil((todosDoneData / all) * 100)}
                     title={'Done'}
-                    stat={k}
+                    stat={todosDoneData}
+                    progress={true}
                     icon={<AiOutlineFileDone size={'3em'} />}
                     iconColor={'whatsapp.400'}
                 />
